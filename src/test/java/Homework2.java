7cfd63ec6fa9cdf3592469b97e2ddc62ad8ba36c
danilfs <62214743+danilfs@users.noreply.github.com>
2021-11-01 11:42:52 +0300

    Initial commit

---
+++ src/test/java/Homework2.java
@@ -0,0 +1,44 @@
+
+import java.util.Arrays;
+
+public class Homework2 {
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        anotherArray (arr);
+        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(fillArrayWithStep(8, 3)));
+        int[] arr1 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        workWithSix(arr1);
+        System.out.println(Arrays.toString(arr1));
+
+
+    }
+
+    //Задать целочисленный массив, состоящий из элементов 0 и 1. Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ].
+    //С помощью цикла и условия заменить 0 на 1, 1 на 0;
+    static void anotherArray(int[] arr) {
+        if (arr == null) return;
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] ^= 1;
+        }
+    }
+
+    //Задать пустой целочисленный массив размером 8. С помощью цикла заполнить его значениями 0 3 6 9 12 15 18 21;
+
+    static int[] fillArrayWithStep(int size, int step) {
+        int[] arr = new int[size];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i * step;
+        }
+        return arr;
+    }
+
+    //Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ] пройти по нему циклом, и числа меньшие 6 умножить на 2;
+    static void workWithSix(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 6) arr[i] *= 2; //arr[i] = arr[i] * 2
+        }
+    }
+
+
+}
---
+++ src/test/java/homework1.java
@@ -0,0 +1,50 @@
+public class homework1 {
+    public static void main(String[] args) {
+        System.out.println ("Задание 1");
+        System.out.println(calculate(50f, 25f, 3f, -4f));
+        System.out.println("Задание 2");
+        System.out.println(numberscheck(10, 1));
+        System.out.println ("Задание 3");
+        numberpositiv(-8);
+        System.out.println ("Задание 4");
+        System.out.println(positivnegativ(-10));
+        welcome("Alo") ;
+    }
+
+
+    // 1 Написать метод вычисляющий выражение a * (b + (c / d)) и возвращающий результат,
+    // где a, b, c, d – аргументы этого метода, имеющие тип float.
+    static float calculate(float a, float b, float c, float d) {
+        return a * (b + (c / d));
+    }
+
+    // 2 Написать метод, принимающий на вход два целых числа и проверяющий, что их сумма лежит в пределах от 10 до 20
+    // (включительно), если да – вернуть true, в противном случае – false.
+    static boolean numberscheck(int a, int b) {
+        int sum = a + b;
+        boolean result = sum <= 20 && sum >= 10;
+        return result;
+    }
+
+    // 3 Написать метод, которому в качестве параметра передается целое число,
+    // метод должен напечатать в консоль, положительное ли число передали или отрицательное.
+    // Замечание: ноль считаем положительным числом.
+    static void numberpositiv(int a) {
+        if (a >= 0) System.out.println( "положительное");
+        else System.out.println(" отрицательное");
+    }
+
+    // 4 Написать метод, которому в качестве параметра передается целое число.
+    // Метод должен вернуть true, если число отрицательное, и вернуть false если положительное.
+    static boolean positivnegativ(int a ) { return a < 0 ;}
+
+
+    //Написать метод, которому в качестве параметра передается строка,
+    // обозначающая имя. Метод должен вывести в консоль сообщение «Привет, указанное_имя!».
+    static String welcome (String name) {
+        String result = ("Привет ," + name + "!");
+        System.out.println (result);
+        return result;
+    }
+}
+
